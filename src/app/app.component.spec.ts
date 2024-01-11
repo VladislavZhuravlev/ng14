de37937580197254b43d9b2ef496f95d2f7dfdc6
VladislavZhuravlev <zhwm.gt@gmail.com>
2024-01-11 12:21:44 +0300

    upd acc 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-//tt
+//tt 22
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
